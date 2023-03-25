833dc26a158f25dfb74bde69519625cce2e2475f
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-03-25 09:17:57 +0100

    Refactoring files

--- scripts/clean-stuck-ns.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-# Ref: #https://stackoverflow.com/a/53661717
-set -euox pipefail
-
-NAMESPACE=$1
-kubectl proxy & kubectl get namespace "$NAMESPACE" -o json | jq '.spec = {"finalizers":[]}' > /tmp/temp.json
-curl -k -H "Content-Type: application/json" -X PUT --data-binary @/tmp/temp.json "127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize"
