baac56425c93a5619c9e5826bea235c7756cbdb9
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-11-10 13:36:04 +0100

    [Make, CD] Make database scripts indepodent

--- make/cbcd/drop.db.sql
+++ make/cbcd/drop.db.sql
@@ -1,7 +1,7 @@
-DROP USER 'flow'@'%';
+DROP USER IF EXISTS 'flow'@'%';
 
-DROP DATABASE flow;
-DROP DATABASE flow_upgrade;
+DROP DATABASE IF EXISTS flow;
+DROP DATABASE IF EXISTS flow_upgrade;
 
 SHOW DATABASES;
 Select user from mysql.user;
